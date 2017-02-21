38725524d5ad7addd2646a3eafccd24f4061b3b8
kevin2b <kevin2b@my.yorku.ca>
2017-02-21 10:51:36 -0500

    Second Version of Player
    Text/Speech not incorporated yet.
    
    Linked the simulator with the Player.  Added a <checkpoint< function for
    the <repeat< function. Added a random scenario generator <random<.
    Partially added a <sfx< function. The Player App will read the the <sfx<
    keyword and isolate a file name, but will not play the audio.

--- audioForPlayerApp/src/player/PlayerApp.java
+++ audioForPlayerApp/src/player/PlayerApp.java
@@ -1,35 +1,56 @@
 package player;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+import javax.swing.JOptionPane;
+
+import simulator.Simulator;
 
 public class PlayerApp {
 	
+	private static final int CELL_NUM = 1;
+	private static final int MAX_BUTTON = 5;
 	private static PlayerApp instance = null;
 	private ArrayList <String> story = new ArrayList <String> ();
+	private Simulator sim;
+	private String numButton;
+	private int buttonActivate;
+	private int buttonPressed;
 	
-	private PlayerApp(String fileName){
+	
+	private PlayerApp(String fileName, int numButton){
 		File file = new File (fileName);
 		StringBuilder sb = new StringBuilder();
 		Scanner input;
 		String line;
 		try {
+			if (numButton == 1 || numButton> MAX_BUTTON){
+				throw new IllegalArgumentException();
+			}
+			sim = new Simulator(CELL_NUM, numButton);
 			input = new Scanner(file);
 			while(input.hasNextLine()){
 				sb.append(input.nextLine()+" ");
 			}
+		} catch (IllegalArgumentException e) {
+			System.out.println("Button number is not greater than one or is greater than " + MAX_BUTTON + ".");
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found.");
 		}
 		line = sb.toString();
-		
-		
-		 for (String part : line.split("<")) {
+		for (String part : line.split("<")) {
 			 part.trim();
 			 story.add(part);
 		 }
+		this.numButton = Integer.toString(numButton);
 		 
 	/*	 System.out.println(line);
 		 for (String part : story) {
@@ -37,9 +58,9 @@ public class PlayerApp {
 		 }*/
 	}
 	
-	public static PlayerApp getPlayerApp(String fileName){
+	public static PlayerApp getPlayerApp(String fileName, int numButton){
 		if (instance == null){
-			instance = new PlayerApp(fileName);
+			instance = new PlayerApp(fileName, numButton);
 		}
 		return instance;
 	}
@@ -49,7 +70,7 @@ public class PlayerApp {
 		for (int counter = 0; counter < story.size();counter++){
 			String line = story.get(counter);
 			
-			if (line.equals("read")){
+			if (line.equals("checkpoint")){
 				checkpoint = counter;
 			}
 			else if (line.equals("repeat")){
@@ -57,12 +78,17 @@ public class PlayerApp {
 					counter = checkpoint;
 				}
 			}
-			else if(line.matches("random[2-5]")){
-				
+			else if(line.matches("random[2-"+ numButton +"]")){
+				int difficulty = Integer.parseInt(line.substring(6, line.length()));
+				boolean pass = randomScenario(difficulty);
+				if (!pass){
+					counter--;
+				}
+				sim.clearAllCells();
 			}
 			else if(line.matches("sfx [A-Za-z]+.wav")){
 				String [] audioClip = line.split(" ");
-				System.out.println(audioClip[1]); //Play Audio
+				System.out.println(audioClip[1]); //Play Audio here instead
 			}
 			
 			else{
@@ -71,15 +97,77 @@ public class PlayerApp {
 		}
 	}
 
+
 	private boolean repeat() {
-		Scanner input = new Scanner(System.in);
-		int i =input.nextInt();
-		if(i==1){
+		int buttonPress = getUserInput(2);
+		if(buttonPress==1){
 			return true;
 		}
 		return false;
 	}
 	
+	private int getUserInput(int numButton){
+		Semaphore semaphore = new Semaphore(0);
+	
+		for (buttonActivate=0;buttonActivate<numButton;buttonActivate++){
+			sim.getButton(buttonActivate).addActionListener(new ActionListener() {
+	
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					buttonPressed = 0;
+					while(e.getSource()!= sim.getButton(buttonPressed)){
+						buttonPressed++;
+					}	
+					semaphore.release();
+				}
+	
+	});
+		}
+		try {
+			semaphore.acquire();
+		} catch (InterruptedException e) {
+		}
+		for(int buttonActivate = 0; buttonActivate < numButton; buttonActivate++){
+			for(ActionListener act : sim.getButton(buttonActivate).getActionListeners()) {
+			sim.getButton(buttonActivate).removeActionListener(act);
+			}
+		}
+		
+		return buttonPressed;
+	}
+	
+	private boolean randomScenario(int difficulty) {
+		int buttonPressed;
+		StringBuilder alphabet = new StringBuilder ("abcdefghijklmnopqrstuvwxyz");
+		ArrayList <Character> option = new ArrayList <Character>();
+		Random random = new Random();
+		int index = random.nextInt(alphabet.length());
+		char correct = alphabet.charAt(index);
+		option.add(correct);
+		alphabet.deleteCharAt(index);
+		for (int counter = 0; counter < difficulty-1; counter++){
+			index = random.nextInt(alphabet.length());
+			option.add(alphabet.charAt(index));
+			alphabet.deleteCharAt(index);
+		}	
+		Collections.shuffle(option);
+		/*for (char c: option){
+		System.out.print(c);
+		}*/
+		for (int counter = 0; counter<option.size();counter++){
+			System.out.println("Press Button " + counter + " if the letter display is " + option.get(counter)); //Text to speech
+		}
+		sim.getCell(0).displayCharacter(correct);
+		buttonPressed = this.getUserInput(difficulty);
+		if (option.get(buttonPressed) == correct){
+			return true;
+		}	
+		return false;
+	}
+	
+	
+	
 	
 	
 }
+
